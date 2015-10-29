897b6422b7cec50c0501c6a90505fab40eef4cc5
Pavel Belousov <pafnuty10@gmail.com>
2015-10-29 15:01:16 +0300

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # tabsToSelect
-Плагин для реализации адаптивных табов, превращающихся в селект
+Плагин для реализации табов, превращающихся в селект с колбэками и адаптивностью.
+
+[пример](http://codepen.io/pafnuty/full/QjrXNg/)
+
+## HTML
+```html
+<div class="tts-tabs">
+    <ul class="tts-tabs-switchers">
+        <li class="tts-tabs-switcher active">таб 1</li>
+        <li class="tts-tabs-switcher">таб 2</li>
+        <li class="tts-tabs-switcher">таб 3</li>
+    </ul>
+
+    <div class="tts-tabs-item active">контент таба 1</div>      
+    <div class="tts-tabs-item">контент таба 2</div> 
+    <div class="tts-tabs-item">контент таба 3</div> 
+</div>
+```
+
+## минимум js для работы плагина 
+`$.tabsToSelect();`
+
+## js (полный пример)
+```js
+$.tabsToSelect({
+    // Класс, добавляемый с селекту
+    selectCalss: 'styler', 
+    // Класс, добавляемый с обёртке селекта
+    selectWrapperCalss: 'styler-wrapper', 
+    onInit: function () {
+       // Срабатывает при инициализации плагина
+    },
+    beforeTabSwich: function (e) {
+        // Срабатывает перед сменой активного таба
+        // Если функция вернёт false - смена таба не произойдёт
+    },
+    beforeTabSwich: function (e) {
+        // Срабатывает после смены активного таба
+    },
+    onResized: function () {
+        // Срабатывает при изменении размера окна
+    },
+});
+```
---
+++ src/js/jquery.tabsToSelect.js
@@ -0,0 +1,163 @@
+/**!
+ * Плагин для реализации адаптивных табов, превращающихся в селект
+ * @link https://github.com/pafnuty/tabsToSelect
+ * @date 27.10.2015
+ * @version 1.0.0
+ * 
+ */
+(function ($, window, document) {
+	'use strict';
+	var pluginName = 'tabsToSelect',
+		previousResizeWidth = 0,
+		$window = $(window),
+		$tabs = $('.tts-tabs'),
+		defaults = {
+			// Класс, добавляемый с селекту
+			selectCalss: '',
+			// Класс, добавляемый с обёртке селекта
+			selectWrapperCalss: '',
+			// Срабатывает при инициализации плагина
+			onInit: function () {},
+			// Срабатывает перед сменой активного таба
+			beforeTabSwich: function (event) {
+				return true;
+			},
+			// Срабатывает после смены активного таба
+			afterTabSwich: function (event) {},
+			// Срабатывает при изменении размера окна
+			onResized: function () {},
+
+		};
+
+	function Plugin(obj, options) {
+		this.settings = $.extend({}, defaults, options);
+		this._defaults = defaults;
+		this.init();
+	}
+
+	$.extend(Plugin.prototype, {
+
+		init: function () {
+			var self = this;
+
+			// Пробегаем по табам
+			$.each($tabs, function () {
+				var $this = $(this), // Текущий блок с табами
+					$select = $('<select class="tts-tabs-select ' + self.settings.selectCalss + '" />'), // Блок с селектом
+					$tabSwitchers = $this.find('.tts-tabs-switcher'), // Переключатели табов
+					$selectInner = []; // Массив для option`s
+
+				// Пробегаем по переключателям табов и формируем селект
+				$.each($tabSwitchers, function (i, tabSwitcher) {
+					var $tabSwitcher = $(tabSwitcher), // Текущий переключатель
+						$data = $tabSwitcher.data(),
+						selected = ($tabSwitcher.hasClass('active')) ? 'selected' : '', // Определяем активный таб
+						option = '<option value="' + i + '" ' + selected + '>' + $tabSwitcher.text() + '</option>'; 
+
+					$selectInner.push(option);
+				});
+
+				$select
+					// Добавляем в селект пункты
+					.html($selectInner.join(''))
+					// Навешиваем обработчик на изменение этого селекта
+					.on('change.' + pluginName, function (e) {
+						// e.flag — для предотвращения рекурсии при клике на переключатель таба
+						if (!e.flag) {
+							var tab = $(this).val() * 1;
+							// Запускаем событие смены активного таба
+							$(this).trigger({
+								type: 'tabSwitch.' + pluginName,
+								tab: tab,
+							});
+						}
+					});
+
+				$this
+					// Вставляем сформированный селект в начало блока с табами
+					.prepend($select)
+					// Навешиваем обработчик на клик по неативным переключателям
+					.on('click.' + pluginName, '.tts-tabs-switcher:not(.active)', function () {
+						var tab = $(this).index();
+						// Запускаем событие смены активного таба
+						$(this).trigger({
+							type: 'tabSwitch.' + pluginName,
+							tab: tab,
+						});
+					})
+					// Навешиваем обработчик для смены активного таба
+					.on('tabSwitch.' + pluginName, function (e) {
+						// Выполняем колбэк перед сменой активного таба
+						var beforeTabSwich = self.settings.beforeTabSwich.call($this, e);
+
+						if (!beforeTabSwich) {
+							return false;
+						};
+
+						var $thisTab = $($tabSwitchers[e.tab]),
+							$tabContent = $thisTab.closest('.tts-tabs').find('.tts-tabs-item'),
+							$thisTabContent = $tabContent.eq($thisTab.index());
+
+						// Меняем активные пункт селекта
+						$select.val(e.tab).trigger({
+							type: 'change',
+							flag: true, // для предотвращения рекурсии при клике на переключатель таба
+						});
+
+						// Меняем активный таб 
+						$thisTab
+							.addClass('active')
+							.siblings().removeClass('active');
+
+						$tabContent.removeClass('active');
+						$thisTabContent.addClass('active');
+
+						$thisTab.trigger({
+							type: 'tabClick.' + pluginName,
+							tab: $thisTabContent
+						});
+
+						// Выполняем колбэк после смены активного таба
+						self.settings.afterTabSwich.call($this, e);
+					});
+				// Заворачиваем селект в контейнер
+				$select.wrap('<div class="tts-tabs-select-wrapper ' + self.settings.selectWrapperCalss + '"></div>');
+			});
+
+			$window.on('resize.' + pluginName + ' orientationchange.' + pluginName, function (event) {
+				self.winResize(event);
+			});
+
+			// Выполняем колбэк после инициализации плагина
+			self.settings.onInit.call($tabs);
+
+
+		},
+
+		winResize: function () {
+			if (event && (event.type === 'resize' || event.type === 'orientationchange')) {
+				var windowWidth = $window.width(),
+					self = this;
+
+				if (windowWidth === previousResizeWidth) {
+					return;
+				}
+
+				// Выполняем колбэк при ресайзе окна
+				self.settings.onResized.call();
+
+				previousResizeWidth = windowWidth;
+			}
+		},
+	});
+
+	if (typeof $[pluginName] === 'undefined') {
+
+		$[pluginName] = function (options) {
+			return new Plugin(this, options);
+		};
+
+	}
+
+
+}(jQuery, window, document));
